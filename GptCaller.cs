2fe46d03b81852e4ba1463bddebaf966418e1a88
ks233 <2681780098@qq.com>
2025-06-04 09:14:19 +0800

    GPT 的 HTTP 代理

--- GptCaller.cs
+++ GptCaller.cs
@@ -1,17 +1,19 @@
 ﻿using OpenAI;
 using OpenAI.Chat;
 using System.ClientModel;
+using System.ClientModel.Primitives;
 using System.Diagnostics;
+using System.Net;
 
 namespace ja_learner
 {
     internal class GptCaller
     {
         private static OpenAIClient client;
-
+        private static OpenAIClientOptions options;
         public static void Initialize()
         {
-            OpenAIClientOptions options = new()
+            options = new()
             {
                 Endpoint = new Uri(Program.APP_SETTING.GPT.ApiUrl)
             };
@@ -26,44 +28,60 @@ namespace ja_learner
         {
             if (useProxy)
             {
-
+                HttpClientHandler handler = new()
+                {
+                    Proxy = new WebProxy(new Uri(Program.APP_SETTING.HttpProxy)),
+                };
+                options = new()
+                {
+                    Endpoint = new Uri(Program.APP_SETTING.GPT.ApiUrl),
+                    Transport = new HttpClientPipelineTransport(new HttpClient(handler))
+                };
+                client = new(new ApiKeyCredential(Program.APP_SETTING.GPT.ApiKey), options);
             }
             else
             {
-
+                options = new()
+                {
+                    Endpoint = new Uri(Program.APP_SETTING.GPT.ApiUrl)
+                };
+                client = new(new ApiKeyCredential(Program.APP_SETTING.GPT.ApiKey), options);
             }
         }
 
         public static async void Chat(string systemPrompt, string userPrompt, Action<string> streamCallback)
         {
-            ChatClient chatClient = client.GetChatClient("gpt-4o-mini");
-            AsyncCollectionResult<StreamingChatCompletionUpdate> completionUpdates = chatClient.CompleteChatStreamingAsync(systemPrompt + "\n" + userPrompt);
-            await foreach (StreamingChatCompletionUpdate completionUpdate in completionUpdates)
+            if (UserConfig.useExtraPrompt)
             {
-                if (completionUpdate.ContentUpdate.Count > 0)
+                systemPrompt += UserConfig.ExtraPrompt;
+            }
+            try
+            {
+                ChatClient chatClient = client.GetChatClient(Program.APP_SETTING.GPT.Model);
+                AsyncCollectionResult<StreamingChatCompletionUpdate> completionUpdates = chatClient.CompleteChatStreamingAsync(systemPrompt + "\n" + userPrompt);
+                await foreach (StreamingChatCompletionUpdate completionUpdate in completionUpdates)
                 {
-                    streamCallback(completionUpdate.ContentUpdate[0].Text);
+                    if (completionUpdate.ContentUpdate.Count > 0)
+                    {
+                        streamCallback(completionUpdate.ContentUpdate[0].Text);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
 
         public static async void CreateTranslateConversation(string userPrompt, Action<string> streamCallback)
         {
             string systemPrompt = Program.APP_SETTING.GPT.TranslatePrompt;
-            if (UserConfig.useExtraPrompt)
-            {
-                systemPrompt += UserConfig.ExtraPrompt;
-            }
             Chat(systemPrompt, userPrompt, streamCallback);
         }
 
         public static async void CreateInterpretConversation(string userPrompt, Action<string> streamCallback)
         {
             string systemPrompt = Program.APP_SETTING.GPT.ExplainPrompt;
-            if (UserConfig.useExtraPrompt)
-            {
-                systemPrompt += UserConfig.ExtraPrompt;
-            }
             Chat(systemPrompt, userPrompt, streamCallback);
         }
     }
